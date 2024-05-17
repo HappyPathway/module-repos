b7f68b4fc95c012c02e6053dc821ad515642fa9c
Dave Arnold <dave@roknsound.com>
2024-05-17 09:11:17 -0700

    moving repo to image-pipeline and adding tfe-workspace

--- main.tf
+++ main.tf
@@ -1,5 +1,16 @@
-module "repo" {
+moved {
+  from = module.repo
+  to = module.aws-image-pipeline
+}
+
+module "aws-image-pipeline" {
   source  = "HappyPathway/module/tfe"
   name = "terraform-aws-image-pipeline"
+  github_is_private = false
+}
+
+module "tfe-workspace" {
+  source  = "HappyPathway/module/tfe"
+  name = "terraform-tfe-workspace"
   github_is_private = false
 }
