dc20442e17a31c09f4683c6b159c403b97c8e053
Dave Arnold <dave@roknsound.com>
2024-08-09 09:01:02 -0700

    terraform-ghe-runner

--- main.tf
+++ main.tf
@@ -15,63 +15,9 @@ locals {
     "terraform-aws-pipeline-iam-role",
     "terraform-aws-pipeline-kms",
     "terraform-aws-pipeline-s3",
-    "terraform-plugin-cache"
+    "terraform-plugin-cache",
+    "terraform-ghe-runner"
   ]
-}
-
-moved {
-  from = module.terraform-plugin-cache
-  to   = module.module["terraform-plugin-cache"]
-}
-
-moved {
-  from = module.terraform-aws-release
-  to   = module.module["terraform-aws-release"]
-}
-
-moved {
-  from = module.aws-image-pipeline
-  to   = module.module["terraform-aws-image-pipeline"]
-}
-
-moved {
-  from = module.tfe-workspace
-  to   = module.module["terraform-tfe-workspace"]
-}
-
-moved {
-  from = module.beanstalk-environment
-  to   = module.module["terraform-aws-beanstalk-environment"]
-}
-
-moved {
-  from = module.terraform-aws-pipeline-codebuild
-  to   = module.module["terraform-aws-pipeline-codebuild"]
-}
-
-moved {
-  from = module.terraform-aws-pipeline-codecommit
-  to   = module.module["terraform-aws-pipeline-codecommit"]
-}
-
-moved {
-  from = module.terraform-aws-pipeline-codepipeline
-  to   = module.module["terraform-aws-pipeline-codepipeline"]
-}
-
-moved {
-  from = module.terraform-aws-pipeline-iam-role
-  to   = module.module["terraform-aws-pipeline-iam-role"]
-}
-
-moved {
-  from = module.terraform-aws-pipeline-kms
-  to   = module.module["terraform-aws-pipeline-kms"]
-}
-
-moved {
-  from = module.terraform-aws-pipeline-s3
-  to   = module.module["terraform-aws-pipeline-s3"]
 }
 
 module "module" {
@@ -82,5 +28,4 @@ module "module" {
   pull_request_bypassers = [
     "/djaboxx"
   ]
-  # github_create_repo = false
 }
