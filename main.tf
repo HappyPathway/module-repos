de2bf9a656e1ef686fb2160ee434f3b2beb7ed3a
Dave Arnold <dave@roknsound.com>
2024-08-22 16:21:06 -0700

    chore: Update module names in main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,86 @@
 locals {
   repos = [
-    "terraform-aws-release",
-    "terraform-aws-image-pipeline",
-    "terraform-tfe-workspace",
-    "terraform-aws-beanstalk-environment",
-    "terraform-aws-pipeline-codebuild",
-    "terraform-aws-pipeline-codecommit",
-    "terraform-aws-pipeline-codepipeline",
-    "terraform-aws-pipeline-iam-role",
-    "terraform-aws-pipeline-kms",
-    "terraform-aws-pipeline-s3",
-    "terraform-plugin-cache",
-    "terraform-ghe-runner",
-    "terraform-importer-gh-actions",
-    "terraform-env-var",
-    "terraform-repo-vars",
-    "terraform-aws-ecr-clone",
-    "terraform-aws-service-account",
-    "terraform-aws-instance"
+    {
+      repo          = "terraform-aws-release",
+      public_module = true,
+    },
+    {
+      repo          = "terraform-aws-image-pipeline",
+      public_module = true,
+    },
+    {
+      repo          = "terraform-tfe-workspace",
+      public_module = true,
+    },
+    {
+      repo          = "terraform-aws-beanstalk-environment",
+      public_module = true,
+    },
+    {
+      repo          = "terraform-aws-pipeline-codebuild",
+      public_module = true,
+    },
+    {
+      repo          = "terraform-aws-pipeline-codecommit",
+      public_module = true,
+    },
+    {
+      repo          = "terraform-aws-pipeline-codepipeline",
+      public_module = true,
+    },
+    {
+      repo          = "terraform-aws-pipeline-iam-role",
+      public_module = true,
+    },
+    {
+      repo          = "terraform-aws-pipeline-kms",
+      public_module = true,
+    },
+    {
+      repo          = "terraform-aws-pipeline-s3",
+      public_module = true,
+    },
+    {
+      repo          = "terraform-plugin-cache",
+      public_module = true,
+    },
+    {
+      repo          = "terraform-ghe-runner",
+      public_module = true,
+    },
+    {
+      repo          = "terraform-importer-gh-actions",
+      public_module = true,
+    },
+    {
+      repo          = "terraform-env-var",
+      public_module = true,
+    },
+    {
+      repo          = "terraform-repo-vars",
+      public_module = true,
+    },
+    {
+      repo          = "terraform-aws-ecr-clone",
+      public_module = false,
+    },
+    {
+      repo          = "terraform-aws-service-account",
+      public_module = true,
+    },
+    {
+      repo          = "terraform-aws-instance",
+      public_module = true,
+    }
   ]
 }
 
 module "module" {
   for_each          = toset(local.repos)
   source            = "HappyPathway/module/tfe"
-  name              = each.value
+  name              = each.value.repo
   github_is_private = false
-  # public_module     = true
+  public_module     = each.value.public_module
   pull_request_bypassers = [
     "/djaboxx"
   ]
